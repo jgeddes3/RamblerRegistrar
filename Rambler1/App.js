0c83708b0ab6a9ea8a7cb59ac74d9b34d80c7d65
jgeddes3 <59842820+jgeddes3@users.noreply.github.com>
2023-05-04 15:00:03 -0500

    It works! but does not look good

--- Rambler1/App.js
+++ Rambler1/App.js
@@ -1,38 +1,19 @@
 // App.js
-import React, { useState } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
-
 import LoginPage from './LoginPage';
 import PageTwo from './PageTwo';
+import PageSix from './PageSix';
 
 const Stack = createStackNavigator();
 
-const loadFonts = () => {
-  return Font.loadAsync({
-    'Cormorant Garamond': require('./assets/Fonts/CormorantGaramond-Regular.ttf'),
-  });
-};
-
 const App = () => {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  if (!fontsLoaded) {
-    return (
-      <AppLoading
-        startAsync={loadFonts}
-        onFinish={() => setFontsLoaded(true)}
-        onError={console.warn}
-      />
-    );
-  }
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Home" component={PageSix} />
         <Stack.Screen name="PageTwo" component={PageTwo} />
       </Stack.Navigator>
     </NavigationContainer>
